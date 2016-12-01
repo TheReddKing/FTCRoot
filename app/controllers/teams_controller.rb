d9347189d2840a216d01cbea61139573ee766ea9
Kevin Fang <reddacc@gmail.com>
2016-11-30 20:22:59 -0600

    Search now includes GS definitions

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -21,16 +21,19 @@ class TeamsController < ApplicationController
             @keywords = params[:q].split( /[<>\s\/\-\_\:\"\&\/]/)
             puts "SEARCHING"
             index = Picky::Index.new :quandas do
-                indexing removes_characters: /[^\p{Alpha}\p{Blank}0-9]/i,splits_text_on: /[<>\s\/\-\_\:\"\&\/]/,
+                indexing removes_characters: /[^\p{Alpha}\p{Blank}0-9]/i,splits_text_on: /[\<\>\s\/\-\_\:\"\&\/]/,
                 substitutes_characters_with:        Picky::CharacterSubstituters::WestEuropean.new,
                 stems_with:                         Lingua::Stemmer.new
-                
+
                 category :title, partial:    Picky::Partial::Substring.new(:from => 1)
                 category :question, partial: Picky::Partial::Substring.new(:from => 3)
                 # category :question
             #   category :name
             end
             i = 0
+
+            #FOR EASY SEARCHING             &lt;[^0-9\<h]*(1|2)(?=&gt;)
+            #REPLACE WITH                   $&&gt;$&g
             array = File.read("#{Rails.root}/app/data/forumanswers.txt").split("*-THEREDDKING-*")
             unique = []
             stuff = HTMLEntities.new
@@ -43,6 +46,22 @@ class TeamsController < ApplicationController
                 unique.append(sp[0])
                 i += 1
             end
+            # FOR RULES                     \<[^0-9\<h]*(1|2)(?=\>)
+            # REPLACE width                 $&>$&g
+            rules = File.read("#{Rails.root}/app/data/rules.txt").split("\n")
+            rules.each_with_index do |line, ind|
+                if(line.length == 0)
+                    next
+                end
+                sp = line.split("*-1THEREDDKING-*")
+                if !unique.include?(sp[0])
+                    index.add Quanda.new(i,"DEFINITION: " + sp[0],sp[0] + " " + sp[1])
+                    rules[ind] = sp[0] + "*-1THEREDDKING-*<h2>DEFINITION: " + CGI::escapeHTML(sp[0]) + "</h2><div class='postcontent'>" + sp[1] + "</div>"
+                end
+                unique.append(sp[0])
+                i += 1
+            end
+            array += rules
             searcher = Picky::Search.new index
             results = searcher.search params[:q]
             for ele in results.ids.uniq
