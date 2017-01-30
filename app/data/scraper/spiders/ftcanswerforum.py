7c51060187a4a71e4c8b2fbcca6ebef014d83e78
Kevin Fang <reddacc@gmail.com>
2017-01-30 15:03:09 -0600

    FTC Forum update + Invididual team data changed a little

--- app/data/scraper/spiders/ftcanswerforum.py
+++ app/data/scraper/spiders/ftcanswerforum.py
@@ -2,8 +2,9 @@ from scrapy.spider import BaseSpider
 from scrapy.selector import Selector
 from scrapy.http import Request
 from scrapy.http import HtmlResponse
+import re
 
-datastore = open("data.txt", 'wb')
+datastore = open("forumanswers.txt", 'wb')
 
 class DmozSpider(BaseSpider):
     name = "ftcforum"
@@ -24,7 +25,7 @@ class DmozSpider(BaseSpider):
         for q in questions:
             title = Selector(text=q).xpath("//h2/text()").extract()[0].strip()
             # answer = Selector(text=q).xpath("//div[@class='content']").extract()[0].strip().replace("\n","").replace("\t","")
-            datastore.write(title.encode("utf-8") + "*-1THEREDDKING-*" + q.replace("\n","").replace("\r","").replace("\t","").encode("utf-8") + "*-THEREDDKING-*")
+            datastore.write(title.encode("utf-8") + "*-1THEREDDKING-*" + re.sub(r'(&lt;[^0-9\<h]*(1|2))(?=&gt;)',r'\1&gt;\1g',q.replace("\n","").replace("\r","").replace("\t","")).encode("utf-8") + "*-THEREDDKING-*")
         pages = Selector(text=response.body).xpath('//div[@class="pagination_top"]/form/span/a/@href').extract()
         for p in pages:
             if "javascript" not in p:
